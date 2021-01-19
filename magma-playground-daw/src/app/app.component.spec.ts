2833a32cd03c8c1effb4f5f1f4455a29a03c5105
thirtytech <thirtytech@outlook.com>
2021-01-19 22:28:37 -0100

    reset project - update angular version 11

--- magma-playground-daw/src/app/app.component.spec.ts
+++ magma-playground-daw/src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'MagmaPlayground'`, () => {
+  it(`should have as title 'magma-playground-daw'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('MagmaPlayground');
+    expect(app.title).toEqual('magma-playground-daw');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('MagmaPlayground app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('magma-playground-daw app is running!');
   });
 });
