59ba4bc2c102b36b86ffd63a2c2843a6981dcc02
thirtytech <thirtytech@outlook.com>
2021-01-19 23:47:59 -0100

    added layout components

--- magma-playground-daw/src/app/app.module.ts
+++ magma-playground-daw/src/app/app.module.ts
@@ -2,15 +2,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './layout/header/header.component'
+import { BodyComponent } from './layout/body/body.component'
+import { FooterComponent } from './layout/footer/footer.component'
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+
   ],
   providers: [],
+  exports: [
+
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ magma-playground-daw/src/app/layout/mainmenu/mainmenu.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'main-menu',
+  templateUrl: './mainmenu.component.html',
+  styleUrls: ['./mainmenu.component.css']
+})
+export class MainMenuComponent {
+
+}
