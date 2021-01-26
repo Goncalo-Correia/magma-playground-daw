dd87f570395e631d5d08895a1a771153b173388d
thirtytech <thirtytech@outlook.com>
2021-01-25 23:59:24 -0100

    troubleshoot issues, started layout

--- magma-playground-daw/src/app/app.module.ts
+++ magma-playground-daw/src/app/app.module.ts
@@ -1,17 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './layout/header/header.component'
-import { BodyComponent } from './layout/body/body.component'
-import { FooterComponent } from './layout/footer/footer.component'
+import { HeaderComponent } from './layout/header/header.component';
+import { BodyComponent } from './layout/body/body.component';
+import { FooterComponent } from './layout/footer/footer.component';
+import { MainMenuComponent } from './layout/mainmenu/mainmenu.component';
+import { SideMenuComponent } from './layout/sidemenu/sidemenu.component';
+import { ResizeMenuComponent } from './layout/resizemenu/resizemenu.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    BodyComponent,
+    FooterComponent,
+    MainMenuComponent,
+    SideMenuComponent,
+    ResizeMenuComponent
   ],
   imports: [
-
+    BrowserModule
   ],
   providers: [],
   exports: [
--- magma-playground-daw/src/app/layout/header/header.component.ts
+++ magma-playground-daw/src/app/layout/header/header.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'layout-header',
-  templateUrl: './header.component.css',
+  templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent {
