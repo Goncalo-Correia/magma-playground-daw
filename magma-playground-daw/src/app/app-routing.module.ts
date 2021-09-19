78f1d1209836a501558b977027b7e3cbab44b442
thirtytech <thirtytech@outlook.com>
2021-09-19 23:26:25 +0000

    added router-outlet components: dashboard and daw

--- magma-playground-daw/src/app/app-routing.module.ts
+++ magma-playground-daw/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { DawComponent } from './daw/daw.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: "", component: DashboardComponent},
+  {path: "daw", component: DawComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ magma-playground-daw/src/app/daw/daw.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'daw',
+    templateUrl: './daw.component.html',
+    styleUrls: ['./daw.component.css']
+})
+
+export class DawComponent {
+
+}
